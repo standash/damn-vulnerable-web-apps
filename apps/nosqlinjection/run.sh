644f5f46a5530e04d4da77e59a4c76a8c5d708b0
standash <stdashevsky@gmail.com>
2019-09-19 11:03:48 +0200

    Add one more app

---
+++ apps/nosqlinjection/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+nodejs &
+
+export logpath=/var/log/mongodb
+export logfile=$logpath/mongodb.log
+touch $logfile
+mongod --quiet --logpath $logfile --logappend &
+
+COUNTER=0
+grep -q 'waiting for connections on port' $logfile
+while [[ $? -ne 0 && $COUNTER -lt 10 ]] ; do
+    sleep 1
+    let COUNTER+=1
+    grep -q 'waiting for connections on port' $logfile
+done
+
+mongoimport --db nosqlInj --collection users --jsonArray mongoData.json 
+nodejs /var/www/server.js 
